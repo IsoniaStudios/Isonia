8c7de7c2813420580c4ccd57786859419ff342f7
Simon <Zhyosgames@gmail.com>
2024-06-06 14:50:41 +0200

    testing

--- Isonia/Math/Interpolation.h
+++ Isonia/Math/Interpolation.h
@@ -14,9 +14,9 @@ namespace Isonia::Math
 
     static inline float Lerp(const float a, const float b, const float t) { return a + t * (b - a); }
 
-    static inline Math::Vector3 Lerp(const Math::Vector3 a, const Math::Vector3 b, const float t)
+    static inline Vector3 Lerp(const Vector3 a, const Vector3 b, const float t)
     {
-        return Math::Vector3(
+        return Vector3(
             Lerp(a.x, b.x, t),
             Lerp(a.y, b.y, t),
             Lerp(a.z, b.z, t)
@@ -29,7 +29,7 @@ namespace Isonia::Math
         return t * t * t * p + t * t * ((a - b) - p) + t * (c - a) + b;
     }
 
-    static inline Math::Vector3 ComputeSmoothNormalFrom4(const float* heightMap, const float dx, const float dz, const uint32_t width, const uint32_t height, const uint32_t z, const uint32_t x)
+    static inline Vector3 ComputeSmoothNormalFrom4(const float* heightMap, const float dx, const float dz, const uint32_t width, const uint32_t height, const uint32_t z, const uint32_t x)
     {
         const uint32_t middleIndex = z * width + x;
         const float top = heightMap[middleIndex - width];
@@ -37,14 +37,14 @@ namespace Isonia::Math
         const float right = heightMap[middleIndex + 1];
         const float bottom = heightMap[middleIndex + width];
 
-        return Math::Normalize(Math::Vector3{
+        return Normalize(Vector3{
             bottom - top,
             -dx - dz,
             right - left,
         });
     }
 
-    static inline Math::Vector3 ComputeSmoothNormalFrom8(const float* heightMap, const float dx, const float dz, const uint32_t width, const uint32_t height, const uint32_t z, const uint32_t x)
+    static inline Vector3 ComputeSmoothNormalFrom8(const float* heightMap, const float dx, const float dz, const uint32_t width, const uint32_t height, const uint32_t z, const uint32_t x)
     {
         const uint32_t middleIndex = z * width + x;
         const float top = heightMap[middleIndex - width];
@@ -56,7 +56,7 @@ namespace Isonia::Math
         const float bottomLeft = heightMap[middleIndex + width - 1];
         const float bottomRight = heightMap[middleIndex + width + 1];
 
-        return Math::Normalize(Math::Vector3{
+        return Normalize(Vector3{
             topRight + right + bottomRight - topLeft - left - bottomLeft,
             -dx - dz - dx - dz,
             bottomLeft + bottom + bottomRight - topLeft - top - topRight
--- Isonia/Math/Matrix.h
+++ Isonia/Math/Matrix.h
@@ -9,77 +9,6 @@ namespace Isonia::Math
 	typedef Matrix<3, 3> Matrix3x3;
 	typedef Matrix<4, 4> Matrix4x4;
 
-	static inline Matrix3x3 Inverse(Matrix3x3 const& m)
-	{
-		float determinant = (
-			+ m[0][0] * (m[1][1] * m[2][2] - m[2][1] * m[1][2])
-			- m[1][0] * (m[0][1] * m[2][2] - m[2][1] * m[0][2])
-			+ m[2][0] * (m[0][1] * m[1][2] - m[1][1] * m[0][2])
-			);
-		float oneOverDeterminant = 1.0f / determinant;
-
-		return Matrix3x3{
-			+(m[1][1] * m[2][2] - m[2][1] * m[1][2]) * oneOverDeterminant,
-			-(m[1][0] * m[2][2] - m[2][0] * m[1][2]) * oneOverDeterminant,
-			+(m[1][0] * m[2][1] - m[2][0] * m[1][1]) * oneOverDeterminant,
-			-(m[0][1] * m[2][2] - m[2][1] * m[0][2]) * oneOverDeterminant,
-			+(m[0][0] * m[2][2] - m[2][0] * m[0][2]) * oneOverDeterminant,
-			-(m[0][0] * m[2][1] - m[2][0] * m[0][1]) * oneOverDeterminant,
-			+(m[0][1] * m[1][2] - m[1][1] * m[0][2]) * oneOverDeterminant,
-			-(m[0][0] * m[1][2] - m[1][0] * m[0][2]) * oneOverDeterminant,
-			+(m[0][0] * m[1][1] - m[1][0] * m[0][1]) * oneOverDeterminant
-		};
-	}
-
-	static inline Matrix4x4 Inverse(Matrix4x4 const& m)
-	{
-		float a2323 = m[2][2] * m[3][3] - m[2][3] * m[3][2];
-		float a1323 = m[2][1] * m[3][3] - m[2][3] * m[3][1];
-		float a1223 = m[2][1] * m[3][2] - m[2][2] * m[3][1];
-		float a0323 = m[2][0] * m[3][3] - m[2][3] * m[3][0];
-		float a0223 = m[2][0] * m[3][2] - m[2][2] * m[3][0];
-		float a0123 = m[2][0] * m[3][1] - m[2][1] * m[3][0];
-		float a2313 = m[1][2] * m[3][3] - m[1][3] * m[3][2];
-		float a1313 = m[1][1] * m[3][3] - m[1][3] * m[3][1];
-		float a1213 = m[1][1] * m[3][2] - m[1][2] * m[3][1];
-		float a2312 = m[1][2] * m[2][3] - m[1][3] * m[2][2];
-		float a1312 = m[1][1] * m[2][3] - m[1][3] * m[2][1];
-		float a1212 = m[1][1] * m[2][2] - m[1][2] * m[2][1];
-		float a0313 = m[1][0] * m[3][3] - m[1][3] * m[3][0];
-		float a0213 = m[1][0] * m[3][2] - m[1][2] * m[3][0];
-		float a0312 = m[1][0] * m[2][3] - m[1][3] * m[2][0];
-		float a0212 = m[1][0] * m[2][2] - m[1][2] * m[2][0];
-		float a0113 = m[1][0] * m[3][1] - m[1][1] * m[3][0];
-		float a0112 = m[1][0] * m[2][1] - m[1][1] * m[2][0];
-
-		float determinant = (
-			+ m[0][0] * (m[1][1] * a2323 - m[1][2] * a1323 + m[1][3] * a1223)
-			- m[0][1] * (m[1][0] * a2323 - m[1][2] * a0323 + m[1][3] * a0223)
-			+ m[0][2] * (m[1][0] * a1323 - m[1][1] * a0323 + m[1][3] * a0123)
-			- m[0][3] * (m[1][0] * a1223 - m[1][1] * a0223 + m[1][2] * a0123)
-			);
-		float oneOverDeterminant = 1 / determinant;
-
-		return Matrix4x4{
-			oneOverDeterminant * (m[1][1] * a2323 - m[1][2] * a1323 + m[1][3] * a1223),
-			oneOverDeterminant * -(m[0][1] * a2323 - m[0][2] * a1323 + m[0][3] * a1223),
-			oneOverDeterminant * (m[0][1] * a2313 - m[0][2] * a1313 + m[0][3] * a1213),
-			oneOverDeterminant * -(m[0][1] * a2312 - m[0][2] * a1312 + m[0][3] * a1212),
-			oneOverDeterminant * -(m[1][0] * a2323 - m[1][2] * a0323 + m[1][3] * a0223),
-			oneOverDeterminant * (m[0][0] * a2323 - m[0][2] * a0323 + m[0][3] * a0223),
-			oneOverDeterminant * -(m[0][0] * a2313 - m[0][2] * a0313 + m[0][3] * a0213),
-			oneOverDeterminant * (m[0][0] * a2312 - m[0][2] * a0312 + m[0][3] * a0212),
-			oneOverDeterminant * (m[1][0] * a1323 - m[1][1] * a0323 + m[1][3] * a0123),
-			oneOverDeterminant * -(m[0][0] * a1323 - m[0][1] * a0323 + m[0][3] * a0123),
-			oneOverDeterminant * (m[0][0] * a1313 - m[0][1] * a0313 + m[0][3] * a0113),
-			oneOverDeterminant * -(m[0][0] * a1312 - m[0][1] * a0312 + m[0][3] * a0112),
-			oneOverDeterminant * -(m[1][0] * a1223 - m[1][1] * a0223 + m[1][2] * a0123),
-			oneOverDeterminant * (m[0][0] * a1223 - m[0][1] * a0223 + m[0][2] * a0123),
-			oneOverDeterminant * -(m[0][0] * a1213 - m[0][1] * a0213 + m[0][2] * a0113),
-			oneOverDeterminant * (m[0][0] * a1212 - m[0][1] * a0212 + m[0][2] * a0112)
-		};
-	}
-
 	template <>
 	struct Matrix<3, 3>
 	{
@@ -106,7 +35,7 @@ namespace Isonia::Math
 			const float x2, const float y2, const float z2
 		) : value{ Vector3{x0, y0, z0}, Vector3{x1, y1, z1}, Vector3{x2, y2, z2} } { }
 		constexpr Matrix(const Vector3& v0, const Vector3& v1, const Vector3& v2) : value{ Vector3(v0), Vector3(v1), Vector3(v2) } { }
-		constexpr Matrix(const Matrix<4, 4>& m) : value{ Vector3(m[0]), Vector3(m[1]), Vector3(m[2]) } { }
+		constexpr Matrix(const Matrix4x4& m) : value{ Vector3(m[0]), Vector3(m[1]), Vector3(m[2]) } { }
 
 		constexpr Matrix3x3& operator=(const Matrix3x3& m)
 		{
@@ -202,9 +131,53 @@ namespace Isonia::Math
 			return Matrix3x3(-m[0], -m[1], -m[2]);
 		}
 
+		constexpr Matrix3x3 Inverse(Matrix3x3 const& m)
+		{
+			float determinant = (
+				+ m[0][0] * (m[1][1] * m[2][2] - m[2][1] * m[1][2])
+				- m[1][0] * (m[0][1] * m[2][2] - m[2][1] * m[0][2])
+				+ m[2][0] * (m[0][1] * m[1][2] - m[1][1] * m[0][2])
+				);
+			float oneOverDeterminant = 1.0f / determinant;
+
+			return Matrix3x3{
+				+(m[1][1] * m[2][2] - m[2][1] * m[1][2]) * oneOverDeterminant,
+				-(m[1][0] * m[2][2] - m[2][0] * m[1][2]) * oneOverDeterminant,
+				+(m[1][0] * m[2][1] - m[2][0] * m[1][1]) * oneOverDeterminant,
+				-(m[0][1] * m[2][2] - m[2][1] * m[0][2]) * oneOverDeterminant,
+				+(m[0][0] * m[2][2] - m[2][0] * m[0][2]) * oneOverDeterminant,
+				-(m[0][0] * m[2][1] - m[2][0] * m[0][1]) * oneOverDeterminant,
+				+(m[0][1] * m[1][2] - m[1][1] * m[0][2]) * oneOverDeterminant,
+				-(m[0][0] * m[1][2] - m[1][0] * m[0][2]) * oneOverDeterminant,
+				+(m[0][0] * m[1][1] - m[1][0] * m[0][1]) * oneOverDeterminant
+			};
+		}
+
 	private:
 		Vector3 value[3];
 	};
+
+	constexpr Matrix3x3 Inverse(Matrix3x3 const& m)
+	{
+		float determinant = (
+			+m[0][0] * (m[1][1] * m[2][2] - m[2][1] * m[1][2])
+			- m[1][0] * (m[0][1] * m[2][2] - m[2][1] * m[0][2])
+			+ m[2][0] * (m[0][1] * m[1][2] - m[1][1] * m[0][2])
+			);
+		float oneOverDeterminant = 1.0f / determinant;
+
+		return Matrix3x3{
+			+(m[1][1] * m[2][2] - m[2][1] * m[1][2]) * oneOverDeterminant,
+			-(m[1][0] * m[2][2] - m[2][0] * m[1][2]) * oneOverDeterminant,
+			+(m[1][0] * m[2][1] - m[2][0] * m[1][1]) * oneOverDeterminant,
+			-(m[0][1] * m[2][2] - m[2][1] * m[0][2]) * oneOverDeterminant,
+			+(m[0][0] * m[2][2] - m[2][0] * m[0][2]) * oneOverDeterminant,
+			-(m[0][0] * m[2][1] - m[2][0] * m[0][1]) * oneOverDeterminant,
+			+(m[0][1] * m[1][2] - m[1][1] * m[0][2]) * oneOverDeterminant,
+			-(m[0][0] * m[1][2] - m[1][0] * m[0][2]) * oneOverDeterminant,
+			+(m[0][0] * m[1][1] - m[1][0] * m[0][1]) * oneOverDeterminant
+		};
+	}
 
 	constexpr Matrix3x3 operator+(const Matrix3x3& m, const float scalar)
 	{
@@ -256,36 +229,16 @@ namespace Isonia::Math
 	}
 	constexpr Matrix3x3 operator*(const Matrix3x3& m1, const Matrix3x3& m2)
 	{
-		float a00 = m1[0][0];
-		float a01 = m1[0][1];
-		float a02 = m1[0][2];
-		float a10 = m1[1][0];
-		float a11 = m1[1][1];
-		float a12 = m1[1][2];
-		float a20 = m1[2][0];
-		float a21 = m1[2][1];
-		float a22 = m1[2][2];
-
-		float b00 = m2[0][0];
-		float b01 = m2[0][1];
-		float b02 = m2[0][2];
-		float b10 = m2[1][0];
-		float b11 = m2[1][1];
-		float b12 = m2[1][2];
-		float b20 = m2[2][0];
-		float b21 = m2[2][1];
-		float b22 = m2[2][2];
-
 		return Matrix3x3{
-			a00 * b00 + a10 * b01 + a20 * b02,
-			a01 * b00 + a11 * b01 + a21 * b02,
-			a02 * b00 + a12 * b01 + a22 * b02,
-			a00 * b10 + a10 * b11 + a20 * b12,
-			a01 * b10 + a11 * b11 + a21 * b12,
-			a02 * b10 + a12 * b11 + a22 * b12,
-			a00 * b20 + a10 * b21 + a20 * b22,
-			a01 * b20 + a11 * b21 + a21 * b22,
-			a02 * b20 + a12 * b21 + a22 * b22
+			m1[0][0] * m2[0][0] + m1[1][0] * m2[0][1] + m1[2][0] * m2[0][2],
+			m1[0][1] * m2[0][0] + m1[1][1] * m2[0][1] + m1[2][1] * m2[0][2],
+			m1[0][2] * m2[0][0] + m1[1][2] * m2[0][1] + m1[2][2] * m2[0][2],
+			m1[0][0] * m2[1][0] + m1[1][0] * m2[1][1] + m1[2][0] * m2[1][2],
+			m1[0][1] * m2[1][0] + m1[1][1] * m2[1][1] + m1[2][1] * m2[1][2],
+			m1[0][2] * m2[1][0] + m1[1][2] * m2[1][1] + m1[2][2] * m2[1][2],
+			m1[0][0] * m2[2][0] + m1[1][0] * m2[2][1] + m1[2][0] * m2[2][2],
+			m1[0][1] * m2[2][0] + m1[1][1] * m2[2][1] + m1[2][1] * m2[2][2],
+			m1[0][2] * m2[2][0] + m1[1][2] * m2[2][1] + m1[2][2] * m2[2][2]
 		};
 	}
 	constexpr Matrix3x3 operator/(const Matrix3x3& m, const float scalar)
@@ -450,9 +403,107 @@ namespace Isonia::Math
 			return Matrix4x4(-m[0], -m[1], -m[2], -m[3]);
 		}
 
+		constexpr Matrix4x4 Inverse(Matrix4x4 const& m)
+		{
+			float a2323 = m[2][2] * m[3][3] - m[2][3] * m[3][2];
+			float a1323 = m[2][1] * m[3][3] - m[2][3] * m[3][1];
+			float a1223 = m[2][1] * m[3][2] - m[2][2] * m[3][1];
+			float a0323 = m[2][0] * m[3][3] - m[2][3] * m[3][0];
+			float a0223 = m[2][0] * m[3][2] - m[2][2] * m[3][0];
+			float a0123 = m[2][0] * m[3][1] - m[2][1] * m[3][0];
+			float a2313 = m[1][2] * m[3][3] - m[1][3] * m[3][2];
+			float a1313 = m[1][1] * m[3][3] - m[1][3] * m[3][1];
+			float a1213 = m[1][1] * m[3][2] - m[1][2] * m[3][1];
+			float a2312 = m[1][2] * m[2][3] - m[1][3] * m[2][2];
+			float a1312 = m[1][1] * m[2][3] - m[1][3] * m[2][1];
+			float a1212 = m[1][1] * m[2][2] - m[1][2] * m[2][1];
+			float a0313 = m[1][0] * m[3][3] - m[1][3] * m[3][0];
+			float a0213 = m[1][0] * m[3][2] - m[1][2] * m[3][0];
+			float a0312 = m[1][0] * m[2][3] - m[1][3] * m[2][0];
+			float a0212 = m[1][0] * m[2][2] - m[1][2] * m[2][0];
+			float a0113 = m[1][0] * m[3][1] - m[1][1] * m[3][0];
+			float a0112 = m[1][0] * m[2][1] - m[1][1] * m[2][0];
+
+			float determinant = (
+				+ m[0][0] * (m[1][1] * a2323 - m[1][2] * a1323 + m[1][3] * a1223)
+				- m[0][1] * (m[1][0] * a2323 - m[1][2] * a0323 + m[1][3] * a0223)
+				+ m[0][2] * (m[1][0] * a1323 - m[1][1] * a0323 + m[1][3] * a0123)
+				- m[0][3] * (m[1][0] * a1223 - m[1][1] * a0223 + m[1][2] * a0123)
+				);
+			float oneOverDeterminant = 1 / determinant;
+
+			return Matrix4x4{
+				oneOverDeterminant * (m[1][1] * a2323 - m[1][2] * a1323 + m[1][3] * a1223),
+				oneOverDeterminant * -(m[0][1] * a2323 - m[0][2] * a1323 + m[0][3] * a1223),
+				oneOverDeterminant * (m[0][1] * a2313 - m[0][2] * a1313 + m[0][3] * a1213),
+				oneOverDeterminant * -(m[0][1] * a2312 - m[0][2] * a1312 + m[0][3] * a1212),
+				oneOverDeterminant * -(m[1][0] * a2323 - m[1][2] * a0323 + m[1][3] * a0223),
+				oneOverDeterminant * (m[0][0] * a2323 - m[0][2] * a0323 + m[0][3] * a0223),
+				oneOverDeterminant * -(m[0][0] * a2313 - m[0][2] * a0313 + m[0][3] * a0213),
+				oneOverDeterminant * (m[0][0] * a2312 - m[0][2] * a0312 + m[0][3] * a0212),
+				oneOverDeterminant * (m[1][0] * a1323 - m[1][1] * a0323 + m[1][3] * a0123),
+				oneOverDeterminant * -(m[0][0] * a1323 - m[0][1] * a0323 + m[0][3] * a0123),
+				oneOverDeterminant * (m[0][0] * a1313 - m[0][1] * a0313 + m[0][3] * a0113),
+				oneOverDeterminant * -(m[0][0] * a1312 - m[0][1] * a0312 + m[0][3] * a0112),
+				oneOverDeterminant * -(m[1][0] * a1223 - m[1][1] * a0223 + m[1][2] * a0123),
+				oneOverDeterminant * (m[0][0] * a1223 - m[0][1] * a0223 + m[0][2] * a0123),
+				oneOverDeterminant * -(m[0][0] * a1213 - m[0][1] * a0213 + m[0][2] * a0113),
+				oneOverDeterminant * (m[0][0] * a1212 - m[0][1] * a0212 + m[0][2] * a0112)
+			};
+		}
+
 	private:
 		Vector4 value[4];
 	};
+
+	static constexpr Matrix4x4 Inverse(Matrix4x4 const& m)
+	{
+		float a2323 = m[2][2] * m[3][3] - m[2][3] * m[3][2];
+		float a1323 = m[2][1] * m[3][3] - m[2][3] * m[3][1];
+		float a1223 = m[2][1] * m[3][2] - m[2][2] * m[3][1];
+		float a0323 = m[2][0] * m[3][3] - m[2][3] * m[3][0];
+		float a0223 = m[2][0] * m[3][2] - m[2][2] * m[3][0];
+		float a0123 = m[2][0] * m[3][1] - m[2][1] * m[3][0];
+		float a2313 = m[1][2] * m[3][3] - m[1][3] * m[3][2];
+		float a1313 = m[1][1] * m[3][3] - m[1][3] * m[3][1];
+		float a1213 = m[1][1] * m[3][2] - m[1][2] * m[3][1];
+		float a2312 = m[1][2] * m[2][3] - m[1][3] * m[2][2];
+		float a1312 = m[1][1] * m[2][3] - m[1][3] * m[2][1];
+		float a1212 = m[1][1] * m[2][2] - m[1][2] * m[2][1];
+		float a0313 = m[1][0] * m[3][3] - m[1][3] * m[3][0];
+		float a0213 = m[1][0] * m[3][2] - m[1][2] * m[3][0];
+		float a0312 = m[1][0] * m[2][3] - m[1][3] * m[2][0];
+		float a0212 = m[1][0] * m[2][2] - m[1][2] * m[2][0];
+		float a0113 = m[1][0] * m[3][1] - m[1][1] * m[3][0];
+		float a0112 = m[1][0] * m[2][1] - m[1][1] * m[2][0];
+
+		float determinant = (
+			+m[0][0] * (m[1][1] * a2323 - m[1][2] * a1323 + m[1][3] * a1223)
+			- m[0][1] * (m[1][0] * a2323 - m[1][2] * a0323 + m[1][3] * a0223)
+			+ m[0][2] * (m[1][0] * a1323 - m[1][1] * a0323 + m[1][3] * a0123)
+			- m[0][3] * (m[1][0] * a1223 - m[1][1] * a0223 + m[1][2] * a0123)
+			);
+		float oneOverDeterminant = 1 / determinant;
+
+		return Matrix4x4{
+			oneOverDeterminant * (m[1][1] * a2323 - m[1][2] * a1323 + m[1][3] * a1223),
+			oneOverDeterminant * -(m[0][1] * a2323 - m[0][2] * a1323 + m[0][3] * a1223),
+			oneOverDeterminant * (m[0][1] * a2313 - m[0][2] * a1313 + m[0][3] * a1213),
+			oneOverDeterminant * -(m[0][1] * a2312 - m[0][2] * a1312 + m[0][3] * a1212),
+			oneOverDeterminant * -(m[1][0] * a2323 - m[1][2] * a0323 + m[1][3] * a0223),
+			oneOverDeterminant * (m[0][0] * a2323 - m[0][2] * a0323 + m[0][3] * a0223),
+			oneOverDeterminant * -(m[0][0] * a2313 - m[0][2] * a0313 + m[0][3] * a0213),
+			oneOverDeterminant * (m[0][0] * a2312 - m[0][2] * a0312 + m[0][3] * a0212),
+			oneOverDeterminant * (m[1][0] * a1323 - m[1][1] * a0323 + m[1][3] * a0123),
+			oneOverDeterminant * -(m[0][0] * a1323 - m[0][1] * a0323 + m[0][3] * a0123),
+			oneOverDeterminant * (m[0][0] * a1313 - m[0][1] * a0313 + m[0][3] * a0113),
+			oneOverDeterminant * -(m[0][0] * a1312 - m[0][1] * a0312 + m[0][3] * a0112),
+			oneOverDeterminant * -(m[1][0] * a1223 - m[1][1] * a0223 + m[1][2] * a0123),
+			oneOverDeterminant * (m[0][0] * a1223 - m[0][1] * a0223 + m[0][2] * a0123),
+			oneOverDeterminant * -(m[0][0] * a1213 - m[0][1] * a0213 + m[0][2] * a0113),
+			oneOverDeterminant * (m[0][0] * a1212 - m[0][1] * a0212 + m[0][2] * a0112)
+		};
+	}
 
 	constexpr Matrix4x4 operator+(const Matrix4x4& m, const float scalar)
 	{
@@ -488,27 +539,33 @@ namespace Isonia::Math
 	}
 	constexpr Vector4 operator*(const Matrix4x4& m, const Vector4& v)
 	{
-		Vector4 mov0(v[0]);
-		Vector4 mov1(v[1]);
-		Vector4 mul0 = m[0] * mov0;
-		Vector4 mul1 = m[1] * mov1;
-		Vector4 add0 = mul0 + mul1;
-
-		Vector4 mov2(v[2]);
-		Vector4 mov3(v[3]);
-		Vector4 mul2 = m[2] * mov2;
-		Vector4 mul3 = m[3] * mov3;
-		Vector4 add1 = mul2 + mul3;
-		return add0 + add1;
+		return Vector4(m[0] * v[0] + m[1] * v[1] + m[2] * v[2] + m[3] * v[3]);
 	}
 	constexpr Vector4 operator*(const Vector4& v, const Matrix4x4& m)
 	{
-		return Vector4(glm::dot(m[0], v), glm::dot(m[1], v), glm::dot(m[2], v), glm::dot(m[3], v));
+		return Vector4(Dot(m[0], v), Dot(m[1], v), Dot(m[2], v), Dot(m[3], v));
 	}
-	//constexpr Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2)
-	//{
-
-	//}
+	constexpr Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2)
+	{
+		return Matrix4x4{
+			m1[0][0] * m2[0][0] + m1[0][1] * m2[1][0] + m1[0][2] * m2[2][0] + m1[0][3] * m2[3][0],
+			m1[0][0] * m2[0][1] + m1[0][1] * m2[1][1] + m1[0][2] * m2[2][1] + m1[0][3] * m2[3][1],
+			m1[0][0] * m2[0][2] + m1[0][1] * m2[1][2] + m1[0][2] * m2[2][2] + m1[0][3] * m2[3][2],
+			m1[0][0] * m2[0][3] + m1[0][1] * m2[1][3] + m1[0][2] * m2[2][3] + m1[0][3] * m2[3][3],
+			m1[1][0] * m2[0][0] + m1[1][1] * m2[1][0] + m1[1][2] * m2[2][0] + m1[1][3] * m2[3][0],
+			m1[1][0] * m2[0][1] + m1[1][1] * m2[1][1] + m1[1][2] * m2[2][1] + m1[1][3] * m2[3][1],
+			m1[1][0] * m2[0][2] + m1[1][1] * m2[1][2] + m1[1][2] * m2[2][2] + m1[1][3] * m2[3][2],
+			m1[1][0] * m2[0][3] + m1[1][1] * m2[1][3] + m1[1][2] * m2[2][3] + m1[1][3] * m2[3][3],
+			m1[2][0] * m2[0][0] + m1[2][1] * m2[1][0] + m1[2][2] * m2[2][0] + m1[2][3] * m2[3][0],
+			m1[2][0] * m2[0][1] + m1[2][1] * m2[1][1] + m1[2][2] * m2[2][1] + m1[2][3] * m2[3][1],
+			m1[2][0] * m2[0][2] + m1[2][1] * m2[1][2] + m1[2][2] * m2[2][2] + m1[2][3] * m2[3][2],
+			m1[2][0] * m2[0][3] + m1[2][1] * m2[1][3] + m1[2][2] * m2[2][3] + m1[2][3] * m2[3][3],
+			m1[3][0] * m2[0][0] + m1[3][1] * m2[1][0] + m1[3][2] * m2[2][0] + m1[3][3] * m2[3][0],
+			m1[3][0] * m2[0][1] + m1[3][1] * m2[1][1] + m1[3][2] * m2[2][1] + m1[3][3] * m2[3][1],
+			m1[3][0] * m2[0][2] + m1[3][1] * m2[1][2] + m1[3][2] * m2[2][2] + m1[3][3] * m2[3][2],
+			m1[3][0] * m2[0][3] + m1[3][1] * m2[1][3] + m1[3][2] * m2[2][3] + m1[3][3] * m2[3][3]
+		};
+	}
 	constexpr Matrix4x4 operator/(const Matrix4x4& m, const float scalar)
 	{
 		return Matrix4x4(m[0] / scalar, m[1] / scalar, m[2] / scalar, m[3] / scalar);
